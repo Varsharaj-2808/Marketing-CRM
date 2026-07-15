35da9336df166e8ca42f3a7aa8af8bc0aa073961
Sulabh Varshney <sulabh.varshney@gmail.com>
2026-07-16 03:29:37 +0530

    frontend-dev-2 merged, api health success, frontend loads

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -6,7 +6,6 @@ const algolia = require('../utils/algoliaService');
 const { query } = require('../config/db');
 const { withTransaction } = require('../utils/transactionHelper');
 const { success: wrapSuccess, error: wrapError } = require('../utils/response');
-const { success: wrapSuccess, error: wrapError } = require('../utils/response');
 
 const sanitize = (str) => str.replace(/<[^>]*>/g, '');
 
